9dbbc1bc8f3ee8a12af092365162348ad0470105
karan fartyal <karanfartyaloffice@gmail.com>
2026-01-12 23:32:45 +0530

    add help, clear command

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const inputDiv = document.getElementById("input");
+let inputDiv = document.getElementsByClassName("input")[0];
+const mainScreen = document.getElementsByClassName("main-screen")[0];
 
-const allowedCommmands = ["help", "whoAreYou", "social", "clear", "projects"];
+let blinker = document.getElementsByClassName("blink_me")[0];
+
+let inputEnabled = true;
+
+const map = {
+  help: "you already know what it does",
+  whoAreYou: "Let me tell you about me.",
+  social: "Wanna see my socials ??",
+  clear: "Clear the terminal",
+  projects: "Wanna take a look at some of my projects ?",
+  banner: "Display the banner",
+  email: "You can contact me through my email.",
+};
 
 const removeOneLetter = () => {
   let fullValue = inputDiv.textContent;
-  console.log(fullValue);
 
   fullValue = fullValue.substring(0, fullValue.length - 1);
   inputDiv.textContent = fullValue;
@@ -18,33 +30,127 @@ const removeLastWord = () => {
 };
 
 const putWord = (e) => {
-  console.log(e.key);
   inputDiv.append(e.key);
 };
 
 const decision = (e) => {
-  if (e.key === "Control") return;
+  if (!inputEnabled) return;
+  const key = e.key;
 
-  if (e.ctrlKey && e.key === "Backspace") {
+  if (key === "Control") return;
+
+  if (e.ctrlKey && key === "Backspace") {
     removeLastWord();
     return;
   }
 
-  if (e.key === "Backspace") {
+  if (key === "Backspace") {
     removeOneLetter();
     return;
   }
 
-  if (e.key == "Enter") {
+  if (key == "Enter") {
     whatCommand();
     return;
   }
+
+  const isAlphaNumericOrSpace = /^[a-zA-Z0-9 ]$/.test(key);
+  if (!isAlphaNumericOrSpace) return;
 
   putWord(e);
 };
 
 const whatCommand = () => {
-  console.log("run command");
+  const command = inputDiv.textContent.trim();
+
+  if (command === "help") {
+    insertHelpCommand();
+    return;
+  } else {
+    notFoundCommand();
+    return;
+  }
+};
+
+const clearUpTerminal = () => {
+  inputEnabled = false;
+
+  inputDiv.classList.remove("input");
+  inputDiv.classList.add("text-[#498869]");
+  blinker.classList.add("remove");
+};
+
+const addUpTerminal = () => {
+  const outerDiv = document.createElement("div");
+  outerDiv.classList.add("flex");
+
+  const userTag = document.createElement("p");
+  userTag.textContent = "Visiter@kash.com:~$";
+  userTag.classList.add("text-[#498869]");
+
+  inputDiv = document.createElement("div");
+  inputDiv.classList += "input text-[#73ABAD]";
+
+  blinker = document.createElement("div");
+  blinker.classList += "blink_me w-[10px] h-[17px] mt-1 bg-[#73ABAD]";
+
+  mainScreen.append(outerDiv);
+
+  outerDiv.appendChild(userTag);
+  outerDiv.appendChild(inputDiv);
+  outerDiv.appendChild(blinker);
+
+  inputEnabled = true;
+};
+
+const insertHelpCommand = () => {
+  clearUpTerminal();
+
+  const outerDiv = document.createElement("div");
+  outerDiv.classList += "grid grid-cols-2 p-3 md:w-1/2 my-2"; //for every command i shall write p-3 and all
+
+  const commands = document.createElement("div");
+  commands.classList += "text-[#73ABAD]";
+
+  const whatCommandsDoes = document.createElement("div");
+  whatCommandsDoes.classList += "text-[#B89076]";
+  const entries = Object.entries(map);
+
+  for (let i = 0; i < entries.length; i++) {
+    const command = document.createElement("h1");
+    const ansCommand = document.createElement("h1");
+
+    command.textContent = entries[i][0];
+    ansCommand.textContent = entries[i][1];
+
+    commands.appendChild(command);
+    whatCommandsDoes.appendChild(ansCommand);
+  }
+
+  outerDiv.appendChild(commands);
+  outerDiv.appendChild(whatCommandsDoes);
+
+  mainScreen.append(outerDiv);
+
+  addUpTerminal();
+};
+
+const notFoundCommand = () => {
+  clearUpTerminal();
+
+  const div = document.createElement("div");
+
+  div.classList.add("p-3");
+
+  const h1 = document.createElement("h1");
+  h1.textContent = `Sorry, I Can't find the command you are looking for !😕😕`;
+  h1.classList.add("text-[#B89076]");
+
+  div.appendChild(h1);
+
+  mainScreen.append(div);
+
+  addUpTerminal();
 };
 
 document.addEventListener("keydown", decision);
